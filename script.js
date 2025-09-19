d52ae8ae672f9f099b0a6b8a9ea451de573eb294
SaradaPanigrahy <98700255+SaradaPanigrahy@users.noreply.github.com>
2025-09-19 20:39:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	const chardict={};
+	for (let char of str) {
+		chardict[char]=(chardict[char]||0)+1;
+	}
+	for (let char of str) {
+		if (chardict[char]===1) {
+			return char;
+		}
+	}
+	return null;
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
